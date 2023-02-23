8df59fbe15ed86ddf17dd230884352f4f38a9f4e
MianiQ <108111363+MianiQ@users.noreply.github.com>
2023-02-23 20:07:28 +0800

    Create net.sh

---
+++ net.sh
@@ -0,0 +1,1 @@
+curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && sudo ./nezha.sh install_agent 121.41.45.131 5555 gcQAVuNXSm4UlHPM5J
